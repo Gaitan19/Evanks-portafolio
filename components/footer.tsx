47c02d8f30ba518f821695bf2693c4e10b8d52e4
gaitan19 <kenleyjos619@gmail.com>
2025-05-01 15:25:59 -0600

    feat: update branding and language throughout the application

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,18 +6,17 @@ const Footer = () => {
         <footer className="max-w-3xl mx-auto">
             <div className="md:flex md:justify-between">
                 <div>
-                    <h4 className="text-3xl font-bold my-6 md:my-0">TarreDev</h4>
+                    <h4 className="text-3xl font-bold my-6 md:my-0">KenleyDev</h4>
                 </div>
                 <div className="px-2 flex justify-between md:gap-8 items-center">
-                    <Link href="#about-me">Sobre mi</Link>
+                    <Link href="#about-me">About me</Link>
                     <Link href="#skills">Skills</Link>
-                    <Link href="#services">Servicios</Link>
                     <Link href="#portfolio">Portfolio </Link>
-                    <Link href="#contact">Contacto</Link>
+                    <Link href="#contact">Contact</Link>
                 </div>
             </div>
             <Separator className="my-4" />
-            <div className="text-center">&copy; 2024 | Landing Page by TarreDev</div>
+            <div className="text-center">&copy; 2025 | Landing Page by KenleyDev</div>
         </footer>
     );
 }
--- components/work-experience.tsx
+++ components/work-experience.tsx
@@ -12,19 +12,20 @@ const WorkExperience = () => {
       <div className="mt-10 space-y-8">
         {workExperiences.map((exp) => (
           <div key={exp.id} className="border-l-4 border-primary pl-6">
-            <h3 className="text-xl font-semibold flex items-center text-white">
+            <h3 className="text-xl font-semibold flex items-center text-gray-900 dark:text-white">
               <Briefcase size={24} className="mr-2 text-primary" />
-              {exp.role} <span className="text-gray-300 ml-2">| {exp.company}</span>
+              {exp.role} <span className="text-gray-600 dark:text-gray-300 ml-2">| {exp.company}</span>
             </h3>
-            <p className="text-base text-gray-400">{exp.duration}</p>
+            <p className="text-base text-gray-700 dark:text-gray-400">{exp.duration}</p>
             <ul className="mt-3 list-disc list-inside space-y-2">
               {exp.responsibilities.map((task, index) => (
-                <li key={index} className="text-base text-gray-300">{task}</li>
+                <li key={index} className="text-base text-gray-700 dark:text-gray-300">{task}</li>
               ))}
             </ul>
           </div>
         ))}
       </div>
+
     </div>
   );
 };
